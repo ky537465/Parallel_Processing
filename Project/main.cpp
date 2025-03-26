37e6a513df40cd9260eb6cc2656a0a7864fb0d85
Kylee <154042034+ky537465@users.noreply.github.com>
2025-03-26 21:31:19 +0000

    update

--- Project/main.cpp
+++ Project/main.cpp
@@ -5,8 +5,6 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
-#include <sstream>
 #include <chrono>
 
 #include "animal.hpp"
@@ -52,71 +50,6 @@ vector<Animal> read_animals(ifstream& inputFile) {
     }
 
     return animals_list;
-}
-
-struct TaxonomyNode {
-    string name;
-    map<string, TaxonomyNode*> children;
-
-    TaxonomyNode(string name) : name(name) {}
-};
-
-void insert_into_tree(TaxonomyNode* root, const Animal& animal) {
-    TaxonomyNode* current = root;
-
-    // Inserting Kingdom
-    if (current->children.find(animal.k) == current->children.end()) {
-        current->children[animal.k] = new TaxonomyNode(animal.k);
-    }
-    current = current->children[animal.k];
-
-    // Inserting Phylum
-    if (current->children.find(animal.p) == current->children.end()) {
-        current->children[animal.p] = new TaxonomyNode(animal.p);
-    }
-    current = current->children[animal.p];
-
-    // Inserting Class
-    if (current->children.find(animal.c) == current->children.end()) {
-        current->children[animal.c] = new TaxonomyNode(animal.c);
-    }
-    current = current->children[animal.c];
-
-    // Inserting Order
-    if (current->children.find(animal.o) == current->children.end()) {
-        current->children[animal.o] = new TaxonomyNode(animal.o);
-    }
-    current = current->children[animal.o];
-
-    // Inserting Family
-    if (current->children.find(animal.f) == current->children.end()) {
-        current->children[animal.f] = new TaxonomyNode(animal.f);
-    }
-    current = current->children[animal.f];
-
-    // Inserting Genus
-    if (current->children.find(animal.g) == current->children.end()) {
-        current->children[animal.g] = new TaxonomyNode(animal.g);
-    }
-    current = current->children[animal.g];
-
-    // Inserting Species
-    if (current->children.find(animal.s) == current->children.end()) {
-        current->children[animal.s] = new TaxonomyNode(animal.s);
-    }
-}
-
-void print_tree(TaxonomyNode* root, int dept = 0) {
-    cout << "|--";
-    // TODO: print out animal name and fact.
-    for (int i = 0; i < dept; ++i) {
-        cout << "  ";
-    }
-    cout << root->name << endl;
-
-    for (auto& child : root->children) {
-        print_tree(child.second, dept + 1);
-    }
 }
 
 int main(int argc, char* argv[]) {
